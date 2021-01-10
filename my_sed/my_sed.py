be7c29835b940f583dcf43f1274e9ae4c6aed155
홍준식 <jshong0907@naver.com>
2021-01-10 20:08:06 +0900

    renew

--- my_sed/my_sed.py
+++ my_sed/my_sed.py
@@ -3,18 +3,16 @@ import sys
 
 def my_sed(from_word, to_word, file_name):
     fin = open(file_name, 'r')
-    new_file = []
+    
     while True:
         line = fin.readline()
         if not line: break
         while from_word in line:
             idx = line.index(from_word)
             line = line[:idx] + to_word + line[idx+len(from_word):]
-        new_file.append(line)
+        
+        print(line, end='')
     fin.close()
-    fout = open(file_name, 'w')
-    fout.writelines(new_file)
-    fout.close()
         
 
 
